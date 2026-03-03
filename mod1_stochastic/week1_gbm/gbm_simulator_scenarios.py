d05c90fe932a8777d7ddad624a7d46e4863b9270
RDYTGL <tagle.aep@icloud.com>
2026-03-03 16:43:16 +0800

    Added a base code for GBM simulation

---
+++ mod1_stochastic/week1_gbm/gbm_simulator_scenarios.py
@@ -0,0 +1,34 @@
+# Define libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set initial parameters
+s_0 = 100 
+mu_values = [0.0, 0.10, 0.30, 0.50]
+sigma_values = [0.10, 0.30, 0.60, 1.00]
+T = 1 # time period
+N = 252 # number of steps
+dt = T / N # time step/delta t
+num_paths = 1000 # number of simulations 
+
+# Generating all random draws
+Z = np.random.standard_normal((num_paths, N))
+
+fig, axes = plt.subplots(4, 4, figsize=(16, 12))
+for i, mu in enumerate(mu_values):
+    for j, sigma in enumerate(sigma_values):
+        #  Building price paths
+        prices = np.zeros ((num_paths, N+1))
+        prices[:, 0] = s_0 
+        for t in range(1, N+1):
+            prices[:, t] = prices[:, t-1] * np.exp((mu - 0.5 * sigma ** 2) * dt + sigma * np.sqrt(dt) * Z[:, t-1])
+        # Plotting paths
+        axes[i, j].plot(prices.T, alpha=0.1, color='blue')
+        axes[i,j].set_title(f'mu={mu:.2f}, sigma={sigma:.2f}')
+
+plt.plot(prices.T, alpha=0.1, color='blue')
+plt.suptitle('gbm sim - 1000 paths @ 16 scenarios')
+fig.supxlabel('days')
+fig.supylabel('asset_price')
+plt.tight_layout()
+plt.show()
