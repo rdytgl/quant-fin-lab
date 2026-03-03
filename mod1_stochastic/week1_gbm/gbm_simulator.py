e2386297453bc0ca5b888e32337b28424c342414
RDYTGL <tagle.aep@icloud.com>
2026-03-03 16:01:06 +0800

    Added first GBM simulation

---
+++ mod1_stochastic/week1_gbm/gbm_simulator.py
@@ -0,0 +1,30 @@
+# Define libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set initial parameters
+s_0 = 100 
+mu = 0.10
+sigma = 0.20
+T = 1 # time period
+N = 252 # number of steps
+dt = T / N # time step/delta t
+num_paths = 1000 # number of simulations 
+
+# Generating all random draws
+Z = np.random.standard_normal((num_paths, N))
+
+#  Building price paths
+prices = np.zeros ((num_paths, N+1))
+prices[:, 0] = s_0
+
+for t in range(1, N+1):
+    prices[:, t] = prices[:, t-1] * np.exp((mu - 0.5 * sigma ** 2) * dt + sigma * np.sqrt(dt) * Z[:, t-1])
+
+# Plotting paths
+plt.figure(figsize=(12,6))
+plt.plot(prices.T, alpha=0.1, color='blue')
+plt.title('gbm sim - 1000 paths')
+plt.xlabel('days')
+plt.ylabel('asset_price')
+plt.show()
